04fc404798a6dcc7ee981cc061064c4bd177a41f
dbugging404 <theavinashmishra13@gmail.com>
2024-07-20 18:58:29 +0530

    refactor: Update AppAppBar component layout and menu links

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import IconButton from '@mui/material/IconButton';
 import Link from 'next/link';
 import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import Image from 'next/image';
 import FacebookIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/X';
